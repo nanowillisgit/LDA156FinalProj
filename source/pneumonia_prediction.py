386a2f621a65b4dfadee806ec5a6cb557d923022
Abhijith Vemulapati <abhiv@abhis-mbp.lan>
2020-12-10 17:25:25 -0800

    plot output changes

--- source/pneumonia_prediction.py
+++ source/pneumonia_prediction.py
@@ -10,14 +10,17 @@
 
 
 import os
+import glob
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from scipy import ndimage
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 from PIL import Image
 import time
+import pdb
 
 dim = 220
 n_samples = 750
@@ -31,6 +34,7 @@ np.random.seed(np.int64(time.time()))
 
 #%%
 
+# Edge detection convolution
 def applySobel(im):
     im = im.astype('int32')
     dx = ndimage.sobel(im, 1)  # horizontal derivative
@@ -42,17 +46,17 @@ def applySobel(im):
 #%%
 
 # Open all images in a directory and generate feature matrix X with shape (n_samples, dim**2)
-def getFeatureMatrix(dirname, filenames, path, sample=True):
+def getFeatureMatrix(dirname, filenames, path, sample_size=n_samples):
     filenames_sample = np.array(filenames)[np.random.choice(len(filenames), n_samples, replace='False')]
-    images = list(map(lambda f: Image.open(path + dirname + '/' + f).copy().resize((dim,dim)), filenames_sample))
-    imageArr = map(np.array, images)
+    images = list(map(lambda f: Image.open(f).copy().resize((dim,dim)), filenames_sample))
+    imageArr = map(lambda i: applySobel(np.array(i)), images)
     # Convert any RGB images to grayscale
     imageArr = [0.2126*i[:,:,0] + 0.7152*i[:,:,1] + 0.0722*i[:,:,2] if len(i.shape) == 3 else i for i in imageArr]
     return np.array(imageArr).reshape(len(imageArr), dim**2)
 #%%
 
-normal_files = os.listdir(train_path + 'NORMAL/')
-pneumonia_files = os.listdir(train_path + 'PNEUMONIA/')
+normal_files = glob.glob(train_path + 'NORMAL/*')
+pneumonia_files = glob.glob(train_path + 'PNEUMONIA/*')
 
 # Split pneumonia images into bacterial and viral
 bacterial_p_files = list(filter(lambda f: 'bacteria' in f, pneumonia_files))
@@ -64,10 +68,10 @@ virus_p_files = list(filter(lambda f: 'virus' in f, pneumonia_files))
 normal_features = getFeatureMatrix('NORMAL', normal_files, train_path)
 normal_tags = np.zeros((normal_features.shape[0],))
 
-bac_features = getFeatureMatrix('PNEUMONIA', bacterial_p_files, train_path)
+bac_features = getFeatureMatrix('PNEUMONIA', bacterial_p_files, train_path, sample_size=n_samples/2)
 bac_tags = np.ones((bac_features.shape[0],))
 
-virus_features = getFeatureMatrix('PNEUMONIA', virus_p_files, train_path)
+virus_features = getFeatureMatrix('PNEUMONIA', virus_p_files, train_path,  sample_size=n_samples/2)
 virus_tags = np.ones((virus_features.shape[0],)) + 1
 #%%
 
@@ -88,7 +92,7 @@ transformed_X = np.concatenate((transformed_normal, transformed_pneumonia))
 # Tag transformed data without distinction within pneumonia data
 y_bin = np.concatenate((normal_tags, np.ones((transformed_pneumonia.shape[0]))))
 
-#%%
+# %%
 plt.figure(figsize=(11,8))
 transformed = [transformed_normal, transformed_pneumonia]
 cols = [('Normal','red'),('Pneumonia','blue')]
@@ -96,10 +100,10 @@ for cls, col in zip(transformed, cols):
     plt.scatter(cls[:,0],cls[:,1],color=col[1], label=col[0])
 plt.title('X-Ray Data 2D Projection - Training Set')
 plt.legend(loc = 'lower left')
-#%%
+# %%
 
-normal_test_files = os.listdir(test_path + 'NORMAL/')
-pneumonia_test_files = os.listdir(test_path + 'PNEUMONIA/')
+normal_test_files = glob.glob(test_path + 'NORMAL/*')
+pneumonia_test_files = glob.glob(test_path + 'PNEUMONIA/*')
 
 normal_test_features = getFeatureMatrix('NORMAL', normal_test_files, test_path)
 pneumonia_test_features = getFeatureMatrix('PNEUMONIA', pneumonia_test_files, test_path)
@@ -116,7 +120,19 @@ tags_test = np.concatenate((normal_tags_test, pneumonia_tags_test))
 
 logistic_regress_model = LogisticRegression()
 logistic_regress_model.fit(transformed_X, y_bin)
-accuracy = logistic_regress_model.score(transformed_X_test, tags_test)
+lr_accuracy = logistic_regress_model.score(transformed_X_test, tags_test)
+
+svm_model = SVC()
+svm_model.fit(transformed_X, y_bin)
+svm_accuracy = svm_model.score(transformed_X_test, tags_test)
+
+lda_binary_model = LDA()
+lda_binary_model.fit(transformed_X, y_bin)
+lda_accuracy = lda_binary_model.score(transformed_X_test, tags_test)
+
+print("Logistic Regression Accuracy: " + str(lr_accuracy))
+print("SVC Accuracy: " + str(svm_accuracy))
+print("LDA Accuracy: " + str(lda_accuracy))
 
 #%%
 plt.figure(figsize=(11,8))
@@ -129,10 +145,14 @@ plt.title('X-Ray Data 2D Projection - Testing Set')
 plt.legend(loc = 'lower left')
 #%%
 fig, axes = plt.subplots(2,3)
-imgs = [normal_features[0,:],normal_features[1,:],bac_features[0,:],bac_features[1,:],virus_features[0,:],virus_features[1,:]]
+imgs = [normal_features[0,:],normal_features[0,:],bac_features[0,:],bac_features[0,:],virus_features[0,:],virus_features[0,:]]
 plt.axis('off')
 
-for i in np.arange(2):
-    for j in np.arange(3):
+for j in np.arange(3):
+    for i in np.arange(2):
+        if i == 0: axes[i,j].title.set_text(['Normal','Bacterial','Viral'][j])
+        img = imgs[2*j+i].reshape((dim,dim))
+        if i == 1: img = applySobel(img)
         axes[i,j].axis('off')
-        axes[i,j].imshow(np.reshape(imgs[3*i+j], (220,220), order = 'C'), cmap = 'gray')
+        axes[i,j].imshow(img, cmap = 'gray')
+
